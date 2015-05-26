cdd61f172a3e934f34b375ec8de4290783dc88c0
hetashpatel7 <hetashpatel7@gmail.com>
2015-05-26 00:55:19 -0400

    add hello world content to project

---
+++ Lesson1/scripts/game.ts
@@ -0,0 +1,14 @@
+﻿class person {
+
+    constructor()
+    { }
+  public  saysHello()
+
+  {
+
+  console.log("Hello world")}
+} 
+function init() {
+    var myPerson = new person();
+    myPerson.saysHello();
+}
